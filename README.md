6517ae0a8ffa4bddb708c0991a184e48efb27f88
James Hyatt <james.hyatt@morrisonsplc.co.uk>
2020-12-10 18:16:53 +0000

    feat: first release

---
+++ README.md
@@ -0,0 +1,10 @@
+This component was bootstrapped with [Direflow](https://direflow.io).
+
+# Calculate B T U
+> This project is created using Direflow
+
+```html
+<calculate-b-t-u></calculate-b-t-u>
+```
+
+Use this README to describe your Direflow Component
---
+++ src/direflow-components/calculate-b-t-u/App.tsx
@@ -0,0 +1,131 @@
+import {
+  Cell,
+  Checkbox,
+  FormControl,
+  Grid,
+  HFlow,
+  Radio,
+  TextField,
+  ThemeProvider,
+  VFlow,
+  Heading,
+} from "bold-ui";
+
+import React, { useEffect, useState } from "react";
+import { useFormState } from "react-use-form-state";
+
+interface IProps {
+  componentTitle: string;
+  sampleList: string[];
+}
+
+const App = () => {
+  const [formState, { text, radio, checkbox }] = useFormState();
+  const [area, setArea] = useState(0);
+  const [btu, setBTU] = useState(0);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(JSON.stringify(formState, null, 2));
+  };
+
+  useEffect(() => {
+    const { width, height, length, factor, details } = formState.values;
+    const btu = area * parseInt(factor.slice(2, 3));
+    setArea(width * 3.281 * (length * 3.281) * (height * 3.281));
+    if (details.length) {
+      const sum = details.reduce(
+        (total: any, amount: any) => parseFloat(total) * parseFloat(amount),
+        btu
+      );
+      setBTU(Math.round(sum));
+    } else {
+      setBTU(Math.round(btu));
+    }
+  }, [formState.values, area]);
+
+  return (
+    <ThemeProvider>
+      <VFlow style={{ padding: "1rem" }}>
+        <Heading level={1}>Calculate BTU</Heading>
+        <>
+          <form onSubmit={handleSubmit}>
+            <Grid wrap>
+              <Cell xs={12}>
+                <TextField
+                  {...text("width")}
+                  label="Width(m)"
+                  placeholder="Width(m)"
+                  error={formState.errors.width}
+                  required
+                />
+              </Cell>
+              <Cell xs={12}>
+                <TextField
+                  {...text("length")}
+                  label="Length(m)"
+                  error={formState.errors.length}
+                  placeholder="Length(m)"
+                  required
+                />
+              </Cell>
+              <Cell xs={12}>
+                <TextField
+                  {...text("height")}
+                  label="Height(m)"
+                  error={formState.errors.height}
+                  placeholder="Height(m)"
+                  required
+                />
+              </Cell>
+              <Cell xs={12}>
+                <FormControl
+                  label="Room type"
+                  error={formState.errors.factor}
+                  required
+                >
+                  <HFlow>
+                    <Radio {...radio("factor", "a-5")} label="Lounge" />
+                    <Radio {...radio("factor", "b-4")} label="Bedroom" />
+                    <Radio {...radio("factor", "c-5")} label="Dining Room" />
+                    <Radio {...radio("factor", "d-3")} label="Kitchen" />
+                    <Radio {...radio("factor", "e-3")} label="Hallway" />
+                    <Radio {...radio("factor", "f-3")} label="Bathroom" />
+                  </HFlow>
+                </FormControl>
+              </Cell>
+              <Cell xs={12}>
+                <FormControl label="Details" error={formState.errors.factor}>
+                  <HFlow>
+                    <Checkbox
+                      {...checkbox("details", "0.9")}
+                      label="Double Glazing"
+                    />
+                    <Checkbox
+                      {...checkbox("details", "1.15")}
+                      label="North Facing"
+                    />
+                    <Checkbox
+                      {...checkbox("details", "1.2")}
+                      label="French Windows"
+                    />
+                  </HFlow>
+                </FormControl>
+              </Cell>
+              <Cell xs={12}></Cell>
+              <Cell xs={12}>
+                <h1>
+                  Room volume: {area}
+                  <br />
+                  BTU: {btu}
+                </h1>
+              </Cell>
+            </Grid>
+          </form>
+        </>
+      </VFlow>
+    </ThemeProvider>
+  );
+};
+
+export default App;
